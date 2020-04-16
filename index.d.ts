85af5b0407818a0c8cc61d31fc7196b8539ef165
Alexander Gribochkin <pockomhadzzop@gmail.com>
2020-04-16 17:39:00 +0300

    Updated dependencies

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 import text2speech from "./dist/tts";
 import speech2text from "./dist/stt";
 
-declare module "yandex-speechkit-promise" {
+declare module "yandex-speech-promise" {
   export { text2speech, speech2text };
 }
